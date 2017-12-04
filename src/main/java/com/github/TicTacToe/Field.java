802e5b5a17f8972b11506948e83fc1b292049804
walpio <doksmal@gmail.com>
2017-12-04 23:14:08 +0100

    Symbol and Field class added

--- src/main/java/com/github/TicTacToe/Field.java
+++ src/main/java/com/github/TicTacToe/Field.java
@@ -1,4 +1,12 @@
 package com.github.TicTacToe;
 
 public class Field {
+
+    private final Coordinates coordinates;
+    private Symbol symbol;
+
+    public Field(Coordinates coordinates, Symbol symbol) {
+        this.coordinates = coordinates;
+        this.symbol = symbol;
+    }
 }
--- src/main/java/com/github/TicTacToe/Symbol.java
+++ src/main/java/com/github/TicTacToe/Symbol.java
@@ -1,4 +1,16 @@
 package com.github.TicTacToe;
 
-public class Symbol {
+public enum Symbol {
+
+    CROSS("X"), CIRCLE("O"), DEFAULT("_");
+
+    private final String symbol;
+
+    Symbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
